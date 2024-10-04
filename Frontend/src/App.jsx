afe3b3221ca1c873609e8893963588ea6b66268f
chathunikathammitaarachchi <chathunikathammitaarachchi74@gmail.com>
2024-10-04 19:48:43 +0530

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Products from './Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Products/>
+     
     </>
   )
 }
